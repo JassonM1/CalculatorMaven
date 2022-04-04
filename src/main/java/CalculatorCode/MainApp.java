bf57ef19877bf2c0b21b962d4d6dfbefdfaf1f9d
JassonMendoza <Jassonmendoza1@gmail.com>
2022-04-04 18:00:12 -0400

    First Commit

---
+++ src/main/java/CalculatorCode/MainApp.java
@@ -0,0 +1,12 @@
+package CalculatorCode;
+
+public class MainApp {
+	public static void main(String[] args) {
+	    Calculator c=new Calculator();
+	    
+	    System.out.println("Adding Two Numbers : " +c.add(2, 2));
+	    
+	    System.out.println("Subtracting 2 Numbers : "+ c.subtract(4, 2));
+	}
+	}
+
